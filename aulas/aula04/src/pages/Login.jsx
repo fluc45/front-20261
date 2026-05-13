761bde3a1c6fbac1227126169c84f9f5b0c46a63
Lucas Vieira <vieira.luc@hotmail.com>
2026-05-12 21:14:38 -0300

    adiciona contexto de auth

--- aulas/aula04/src/pages/Login.jsx
+++ aulas/aula04/src/pages/Login.jsx
@@ -1,12 +1,19 @@
-import { useNavigate } from "react-router";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 function Login() {
-    const navigate = useNavigate();
+
+
+  const { login } = useContext(AuthContext);
+
+  const handleEntrar = () => {
+    login({usuario: "eu", senha: "12345"})
+  };
 
   return (
     <>
       <h1>Login</h1>
-      <button onClick={() => navigate("/")} >Entrar</button>
+      <button onClick={handleEntrar}>Entrar</button>
     </>
   );
 }
